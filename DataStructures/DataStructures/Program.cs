facfc32dea7e209eb0f9b78ee32bc5a73080dd1d
scty11 <scotty_wright@my.open.ac.uk>
2015-01-05 13:38:21 +0000

    Added code

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = new CircularBuffer<double>(capacity: 3);
+
+            ProcessInput(buffer);
+
+            SumTheValues(buffer);
+        }
+
+        private static void SumTheValues(CircularBuffer<double> buffer)
+        {
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void ProcessInput(CircularBuffer<double> buffer)
+        {
+            while (true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
